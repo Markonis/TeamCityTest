6082379363a1a2cf0fe4cc1b7da4981e7107c190
Marko Pavlovic <mare.nis@live.com>
2015-07-17 15:51:08 +0000

    Added Test Website

---
+++ TestWebsite/Scripts/custom-lib.js
@@ -0,0 +1,3 @@
+﻿function doSomeWork() {
+    return 5;
+}
---
+++ TestWebsite/Scripts/custom-tests.js
@@ -0,0 +1,6 @@
+﻿describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        expect(doSomeWork()).toBe(6);
+    });
+});
+
